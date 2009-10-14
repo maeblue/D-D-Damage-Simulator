4265edd410eb47db214dabb66985be5c8e3988f5
Andy Ewing <andy.ewing@duke.edu>
2009-10-13 22:27:09 -0400

    Finished die

--- Die.rb
+++ Die.rb
@@ -1,12 +1,33 @@
 #Die Class, for simulating die rolls
 class die
 
-#each die has a number of sides
-attr_accessor :sides
+  #each die has a number of sides
+  attr_accessor :sides
 
-#initialize the die with it's number of sides
-#and possibly number of rolls
+  #initialize the die with it's number of sides
+  #and possibly number of rolls
+  def initialize(num_sides)
+    @sides = num_sides
+  end
 
-#generate the die rolls
+  #generate the die rolls
+  def roll(num_rolls)
+    
+    #holding array
+    rolls = []
+    
+    #for each roll
+    num_rolls.times do |r|
+      
+      #generate a uniformly-distributed random number between 1 and num_sides
+      rolls << rand(num_sides) + 1
+    
+    end
+    
+    return rolls
+    
+  end
+  
+    
 
 end
