901136db65f6f487851fdea15aca5254b528453e
Andy Ewing <andy.ewing@duke.edu>
2009-10-13 21:42:39 -0400

    stubbed out die class

---
+++ Die.rb
@@ -0,0 +1,12 @@
+#Die Class, for simulating die rolls
+class die
+
+#each die has a number of sides
+attr_accessor :sides
+
+#initialize the die with it's number of sides
+#and possibly number of rolls
+
+#generate the die rolls
+
+end
